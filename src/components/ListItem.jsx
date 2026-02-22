0f27a9f8a72be39531085a7df48a26b524859513
disortus <disortus.voidcaster@proton.me>
2026-02-22 21:42:05 +0500

    style correcting

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -4,30 +4,32 @@ import cn from 'classnames';
 
 function ListItem({ item, onToggle, onDelete }) {
   return (
-    <div 
+    <div
       className="p-4 bg-zinc-800 border border-zinc-700/50 rounded-2xl shadow-sm hover:shadow-md transition-all flex items-center justify-between group cursor-pointer"
       onClick={() => onToggle(item.id)}
     >
       <div className="flex items-center gap-4">
         <Check isComplite={item.completed} />
-        <span className={cn("text-lg transition-all duration-300", { 
-          "line-through text-zinc-500": item.completed,
-          "text-white": !item.completed 
-        })}>
+        <span
+          className={cn("text-lg transition-all duration-300", {
+            "line-through text-zinc-500": item.completed,
+            "text-white": !item.completed,
+          })}
+        >
           {item.title}
         </span>
       </div>
-      
+
       <button
         onClick={(e) => {
           e.stopPropagation();
           onDelete(item.id);
         }}
-        className="group-hover:opacity-100 p-2 hover:bg-zinc-700 rounded-xl transition-all duration-500"
+        className="group-hover:opacity-100 p-2 hover:bg-zinc-700 rounded-xl transition-all duration-300 hover:text-rose-400"
       >
         <BsTrashFill
           size={20}
-          className="text-zinc-500 hover:text-rose-400 transition-colors ease-in-out duration-500"
+          className="text-zinc-500 hover:text-rose-400 transition-colors ease-in-out duration-300"
         />
       </button>
     </div>
